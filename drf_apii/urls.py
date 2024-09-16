5c269e86963e5c125905ce2fe3d3ba25c530697f
Lima Zurmati <urzalalima@gmail.com>
2024-09-16 20:01:20 +0000

    Added post views tests

--- drf_apii/urls.py
+++ drf_apii/urls.py
@@ -1,4 +1,4 @@
-"""drf_apii URL Configuration
+"""afghanistan_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
